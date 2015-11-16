ae429fbc526da3399eb4d47903a47322aa1e49c1
Steven Maguire <stevenmaguire@gmail.com>
2015-11-16 17:55:20 -0600

    Add phpdocs and psr2 compliance to resource owner

--- src/Provider/AzureResourceOwner.php
+++ src/Provider/AzureResourceOwner.php
@@ -1,18 +1,35 @@
 <?php
+
 namespace TheNetworg\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
 class AzureResourceOwner implements ResourceOwnerInterface
 {
+    /**
+     * Response payload
+     *
+     * @var array
+     */
     protected $response;
 
-    public function __construct($response = []) {
+    /**
+     * Creates new azure resource owner.
+     *
+     * @param array  $response
+     */
+    public function __construct($response = [])
+    {
         $this->response = $response;
     }
-	
-    public function getId() {
+
+    /**
+     * Retrieves id of azure resource owner.
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
         return $this->response['objectId'] ?: null;
     }
 }
-
